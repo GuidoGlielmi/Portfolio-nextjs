2ff057b57984d45da39abc70c95cac8c4aa35340
GuidoGlielmi <guidoglielmi@gmail.com>
2024-10-11 00:03:38 -0300

    fix: vercel analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,10 @@
-import type {AppProps} from 'next/app';
-import 'styles/globals.css';
-import {useRouter} from 'next/router';
-import LanguageProvider from 'components/contexts/language';
+import {Analytics} from '@vercel/analytics/next';
 import GifPreviewProvider from 'components/contexts/gifPreview';
+import LanguageProvider from 'components/contexts/language';
+import type {AppProps} from 'next/app';
 import Head from 'next/head';
+import {useRouter} from 'next/router';
+import 'styles/globals.css';
 
 export default function App({Component, pageProps}: AppProps) {
   const router = useRouter();
@@ -16,6 +17,7 @@ export default function App({Component, pageProps}: AppProps) {
       <LanguageProvider>
         <GifPreviewProvider>
           <Component {...pageProps} key={router.asPath} />
+          <Analytics />
         </GifPreviewProvider>
       </LanguageProvider>
     </>
