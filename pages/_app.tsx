995b835fb00cf57d5890177ba80c05827365daf3
GuidoGlielmi <guidoglielmi@gmail.com>
2024-10-12 22:27:17 -0300

    feat: added meta tags for SEO and google crawl

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -9,9 +9,30 @@ import 'styles/globals.css';
 
 export default function App({Component, pageProps}: AppProps) {
   const router = useRouter();
+  const jsonLd = {
+    '@context': 'https://schema.org',
+    '@type': 'WebSite',
+    name: 'Guido Glielmi portfolio',
+    url: 'https://www.guidoglielmi.com.ar',
+    description: 'Showcase of portfolio and resume information of Guido Glielmi',
+    logo: 'https://www.example.org/images/my-logo.png',
+  };
+
   return (
     <>
       <Head>
+        <meta property='og:type' content='website' />
+        <meta property='og:title' content='Guido Glielmi portfolio' />
+        <meta
+          property='og:description'
+          content='Showcase of portfolio and resume information of Guido Glielmi'
+        />
+        <meta
+          property='og:image'
+          content='https://guidoglielmi.com.ar/assets/img/profile-img.jpg'
+        />
+        <meta property='og:url' content='https://guidoglielmi.com.ar' />
+
         <meta name='description' content='Portfolio and resume information of Guido Glielmi' />
         <meta
           name='google-site-verification'
@@ -19,6 +40,12 @@ export default function App({Component, pageProps}: AppProps) {
         />
         <meta name='viewport' content='width=device-width, initial-scale=1' />
         <link rel='icon' href='favicon.svg' type='image/svg+xml' />
+
+        <script
+          key='website-jsonld'
+          type='application/ld+json'
+          dangerouslySetInnerHTML={{__html: JSON.stringify(jsonLd)}}
+        />
       </Head>
       <LanguageProvider>
         <GifPreviewProvider>
